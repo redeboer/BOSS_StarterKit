9552f4cec6c8a377c1f18375fc7174022be058bc
Remco de Boer <remco@ihep.ac.cn>
2019-06-14 10:41:00 +0800

    Moved loading of TestRelease to setup/LoadBoss.sh

--- setup/LoadBoss.sh
+++ setup/LoadBoss.sh
@@ -15,5 +15,17 @@ CMTHOME="/afs/ihep.ac.cn/bes3/offline/Boss/cmthome/cmthome-${BOSSVERSION}"
 # * Load BOSS essentials * #
 source "${BOSSINSTALL}/${CMTHOMENAME}/setupCMT.sh"
 source "${BOSSINSTALL}/${CMTHOMENAME}/setup.sh"
+
+# Fetch TestRelease if not available
+if [[ ! -d "${BOSSINSTALL}/workarea/TestRelease" ]]; then
+  currentPath="$(pwd)"
+  cd "${BOSSINSTALL}/workarea"
+  cp -R "${BesArea}/TestRelease" .
+  cd TestRelease/TestRelease-*/cmt
+  cmt broadcast
+  make
+  cd "${currentPath}"
+fi
+
 source "${BOSSINSTALL}/${WORKAREANAME}/TestRelease/TestRelease-"*"/cmt/setup.sh"
 export PATH=$PATH:/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/
--- setup/LoadStarterKit.sh
+++ setup/LoadStarterKit.sh
@@ -2,17 +2,6 @@
 
 export BOSS_StarterKit_OutputDir="${BOSS_StarterKit}/output"
 
-# Fetch TestRelease if not available
-currentPath="$(pwd)"
-if [[ ! -d "${BOSS_StarterKit}/workarea/TestRelease" ]]; then
-  cd "${BOSS_StarterKit}/workarea"
-  cp -R "${BesArea}/TestRelease" .
-  cd TestRelease/TestRelease-*/cmt
-  cmt broadcast
-  make
-  cd "${currentPath}"
-fi
-
 for i in $(find "${BOSS_StarterKit}/workarea" -type f -wholename */setup.sh); do
   source "$i"
 done
