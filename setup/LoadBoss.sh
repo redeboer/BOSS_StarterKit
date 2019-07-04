8f6af220aaf815d95de779c806aebe6e96c0b955
redeboer <remco.de.boer@ihep.ac.cn>
2019-07-04 09:49:08 +0800

    First time loading BOSS asks for version. Version is stored to setup/BOSSVERSION and can be modified there any time

--- setup/LoadBoss.sh
+++ setup/LoadBoss.sh
@@ -1,8 +1,28 @@
 #!/bin/bash -
 # echo "Loading \"${BASH_SOURCE[0]/$(dirname ${BOSS_StarterKit})\/}\""
+BesAreaTemp="/afs/ihep.ac.cn/bes3/offline/Boss"
 
 # * BOSS paths and parameters * #
-export BOSSVERSION="7.0.4"
+BOSSversionFile="$(dirname ${BASH_SOURCE[0]})/BOSSVERSION"
+if [[ -f "${BOSSversionFile}" ]]; then
+  BOSSVERSION=$(head -n 1 "${BOSSversionFile}")
+else
+  echo "It seems this is the first time you load BOSS."
+  read -p "Which version do you want to load? (default: 7.0.4) " BOSSVERSION
+  echo
+  echo "Will load version ${BOSSVERSION} from now on. Change file"
+  echo "  ${BOSSversionFile}"
+  echo "if you want to switch version"
+  echo "${BOSSVERSION}" > ${BOSSversionFile}
+fi
+if [[ ! -d "${BesAreaTemp}/${BOSSVERSION}" ]]; then
+  PrintError "BOSS version ${BOSSVERSION} does not exist!"
+  PrintError "  Check file \"${BOSSversionFile}\""
+  echo "You can find all BOSS versions under ${BesArea}"
+  echo "  ${BesArea}"
+  exit 1
+fi
+export BOSSVERSION
 export CMTHOMENAME="cmthome"
 export WORKAREANAME="workarea"
 
